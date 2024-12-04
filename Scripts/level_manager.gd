2a577b534a05b7f612bde9771c39164f02fd571f
ezekiel342007 <ezekiel342007@gmail.com>
2024-12-04 11:29:28 +0100

    defined level class

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -1,4 +1,12 @@
 extends Node
 
-var current_level: String
+var current_level: Level
+var completed: bool = false
 
+class Level:
+	static var path: String
+	static var completed: bool
+
+	func _init(init_path: String, init_completed: bool):
+		self.path = init_path
+		self.completed = init_completed
